aac1acb764c5043430d863db6c0f8d6ea6b8bcad
fatcorn <bexplorer@163.com>
2019-10-18 11:31:18 +0800

    修改阿里云权限错误问题

--- group-core/utils/src/main/java/com/den/we/AliYunOssUtil.java
+++ group-core/utils/src/main/java/com/den/we/AliYunOssUtil.java
@@ -16,10 +16,10 @@ public class AliYunOssUtil {
 
     public static String updatePicture(InputStream inputStream, String suffix) {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
-        String endpoint = "http://oss-cn-chengdu.aliyuncs.com";
+        String endpoint = "oss-cn-chengdu.aliyuncs.com";
 // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
         String accessKeyId = "LTAI4FisGyijRwBTYCpmdZAr";
-        String accessKeySecret = "Pr4hmohCYNRvcrKj1Vy3YfvYdHZWYn ";
+        String accessKeySecret = "Pr4hmohCYNRvcrKj1Vy3YfvYdHZWYn";
 
         String bucketName = "same-test";
 
