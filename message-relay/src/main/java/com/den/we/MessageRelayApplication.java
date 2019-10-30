9f98738e3dd4650854c842722e38e08766c967e2
fatcorn <bexplorer@163.com>
2019-10-30 17:11:39 +0800

    mqtt 消息中转模块

---
+++ message-relay/src/main/java/com/den/we/MessageRelayApplication.java
@@ -0,0 +1,16 @@
+package com.den.we;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MessageRelayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageRelayApplication.class, args);
+    }
+
+}
